9cec159daad5bc98bbbdb689347314760337292f
ranaabdellatif <ranaabdellatif645@gmail.com>
2025-05-04 17:02:03 -0600

    Revert "go"
    This reverts commit cbd36ceede77b8f1ae14c4697df59aace71230c9.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,4 @@
 
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-import AuthenticatedRoute from './AuthenticatedRoute';
-import ASLWebRecorder from './src/ASLWebRecorder.js';
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route
-            path="/record"
-            element={
-              <AuthenticatedRoute>
-                <ASLWebRecorder />
-              </AuthenticatedRoute>
-            }
-          />
-          {/* Optional: Redirect root to login or recording */}
-          <Route path="*" element={<LoginForm />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
-}
-
-export default App;
-
-
-
-
-/*
 import React from 'react';
 import ASLWebRecorder from './ASLWebRecorder.js';
 
@@ -45,4 +7,3 @@ function App() {
 }
 
 export default App;
-*/
